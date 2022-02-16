84e7a1da0bd095ab5165d56568bf7744eece2613
Nicolas van Kempen <nvankemp@gmail.com>
2022-02-16 15:07:41 -0500

    Working output.

--- src/instruction_counter.cpp
+++ src/instruction_counter.cpp
@@ -6,7 +6,7 @@
 
 static KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "intruction_counter.out",
                                         "specify output file name");
-static std::unordered_map<UINT32, int> Frequencies;
+static std::unordered_map<std::string, int> Frequencies;
 
 VOID OnThreadStart(THREADID ThreadId, CONTEXT* Context, INT32 Flags, VOID* _) {}
 
@@ -15,13 +15,19 @@ VOID OnThreadEnd(THREADID ThreadId, const CONTEXT* Context, INT32 Code, VOID* _)
 VOID OnProgramEnd(INT32 Code, VOID* _) {
   std::ofstream OutputFile;
   OutputFile.open(KnobOutputFile.Value().c_str());
+  for (auto Frequency : Frequencies) {
+    OutputFile << Frequency.first << "," << Frequency.second << std::endl;
+  }
 }
 
-VOID OnInstruction(THREADID ThreadId, UINT32 Code) { ++Frequencies[Code]; }
+VOID OnInstruction(THREADID ThreadId, UINT32 InstructionIndex) {
+  INS Instruction;
+  Instruction.index = (INT32) InstructionIndex;
+  ++Frequencies[INS_Mnemonic(Instruction)]; }
 
 VOID Instruction(INS Instruction, VOID* _) {
   INS_InsertCall(Instruction, IPOINT_BEFORE, (AFUNPTR) OnInstruction, IARG_THREAD_ID, IARG_UINT32,
-                 INS_Opcode(Instruction), IARG_END);
+                 (UINT32) Instruction.index, IARG_END);
 }
 
 INT32 Usage() {
